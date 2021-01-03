a18c748ba5df8f30776d607220349fde4e1bc433
Avaer Kazmer <a@modules.io>
2021-01-03 10:49:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import * as THREE from 'three';
 import {renderer, camera, runtime, world, universe, physics, ui, rig, app, appManager, popovers} from 'app';
 
+const physicsId = physics.addBoxGeometry(new THREE.Vector3(0, -1/2, 0), new THREE.Quaternion(), new THREE.Vector3(1000, 1, 1000), false);
+
+const w = 4;
 const portalSpec = {
   objects: [
     {
@@ -19,3 +22,5 @@ const b = new Blob([s], {
 });
 const u = URL.createObjectURL(b) + '/portal.url';
 world.addObject(u, null, new THREE.Vector3(), new THREE.Quaternion());
+
+console.log('add portal', portalSpec);
